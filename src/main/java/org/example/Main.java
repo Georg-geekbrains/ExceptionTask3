e13d7b549b8ba2ac85b03465a1676115955a507d
mac <georghome1@gmail.com>
2023-09-25 23:01:04 +0300

    Условия задачи

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+ /*
+ Дан следующий код, исправьте его там, где требуется
+ (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+ */
+
 public class Main {
 
         public static void main(String[] args) {
